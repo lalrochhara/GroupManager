c8d3654ba8b8c4ddfaecbcaa501538e519da1c81
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-11-18 02:46:08 +0530

    Create config.py

---
+++ haruka/config.py
@@ -0,0 +1,42 @@
+# Create a new config.py file in same dir and import, then extend this class.
+class Config(object):
+    LOGGER = True
+
+    # REQUIRED
+    API_KEY = "2110670112:AAGyMn2hgU2znqzFwzFa-kM-vaSVcWjLzgE"
+    OWNER_ID = "1382148141"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "Nickylrca"
+
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = 'postgres://grqeohdl:Z20s9CSfgxI-RSkSh7oPU7HQJonYQdb_@otto.db.elephantsql.com/grqeohdl'  # needed for any database modules
+    MESSAGE_DUMP = "-1001371345308"  # needed to make sure 'save from' messages persist
+    LOAD = []
+    NO_LOAD = ['sed']
+    WEBHOOK = False
+    URL = None
+
+    # OPTIONAL
+    SUDO_USERS = "1060318977"  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = "1060318977"  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = "1382148141 1060318977" # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    MAPS_API = False
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
+    STRICT_ANTISPAM = False
+    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
+    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
+    STRICT_GBAN = False
+    STRICT_GMUTE = False
+    ALLOW_EXCL = True  # Allow ! commands as well as /
+    API_OPENWEATHER = "20940ce270c21418fe252bda6a998d50" # OpenWeather API
+
+    # MEMES
+    DEEPFRY_TOKEN = None
+
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
