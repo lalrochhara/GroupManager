9961a8c2eac2a539f25166e439eacadade87eea9
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-11-18 02:40:22 +0530

    Update config.py

--- haruka/config.py
+++ haruka/config.py
@@ -10,23 +10,23 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "2110670112:AAGyMn2hgU2znqzFwzFa-kM-vaSVcWjLzgE"
+    OWNER_ID = "1382148141"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "Nickylrca"
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
-    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
+    SQLALCHEMY_DATABASE_URI = 'postgres://grqeohdl:Z20s9CSfgxI-RSkSh7oPU7HQJonYQdb_@otto.db.elephantsql.com/grqeohdl'  # needed for any database modules
+    MESSAGE_DUMP = "-1001371345308"  # needed to make sure 'save from' messages persist
     LOAD = []
-    NO_LOAD = ['translation', 'sed']
+    NO_LOAD = ['sed']
     WEBHOOK = False
     URL = None
 
     # OPTIONAL
-    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    MAPS_API = ''
+    SUDO_USERS = [1060318977]  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = [1060318977]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = [1382148141 1060318977]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    MAPS_API = False
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
@@ -36,7 +36,7 @@ class Config(object):
     STRICT_GBAN = False
     STRICT_GMUTE = False
     ALLOW_EXCL = True  # Allow ! commands as well as /
-    API_OPENWEATHER = None # OpenWeather API
+    API_OPENWEATHER = "20940ce270c21418fe252bda6a998d50" # OpenWeather API
 
     # MEMES
     DEEPFRY_TOKEN = None
