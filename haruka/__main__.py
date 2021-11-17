7b610ef78c7fefcf17d307d7308bac3b62300612
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-11-18 03:14:48 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -658,7 +658,8 @@ def process_update(self, update):
             self.logger.exception('An uncaught error was raised while processing the update')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
-    LOGGER.info("Successfully loaded")
+    telethn.start(bot_token=TOKEN)
+    pbot.start()
     main()
