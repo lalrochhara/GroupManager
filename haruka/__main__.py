a66783557b644bd161615fdfe9f22cbe13fad3c3
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-11-18 09:28:04 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -660,4 +660,5 @@ def process_update(self, update):
 
 if __name__ == '__main__':
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
+    LOGGER.info("Successfully loaded")
     main()
